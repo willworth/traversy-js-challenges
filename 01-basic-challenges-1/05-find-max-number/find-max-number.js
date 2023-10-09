02f9f3a5228f2ff045e2185420be08314cd92adc
Will <will.worth@gmail.com>
2023-10-09 12:00:25 +0200

    s

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numArr) {
+  let max = numArr[0];
+  for (const elem of numArr) {
+    if (elem > max) {
+      max = elem;
+    }
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;
--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,11 @@
-function titleCase() {}
+function titleCase(wordsString) {
+  const words = wordsString.split(" ");
+
+  const capitalisedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
+  const capitalisedString = capitalisedWords.join(" ");
+  return capitalisedString;
+}
 
 module.exports = titleCase;
