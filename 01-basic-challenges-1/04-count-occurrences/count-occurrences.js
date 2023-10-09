e9aa98dcd1e8db8d80218e5d1baf338ac5622413
Will <will.worth@gmail.com>
2023-10-09 10:57:38 +0200

    s

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,31 @@
-function countOccurrences() {}
+function countOccurrences(input, testChar) {
+  return Array.from(input).filter((currentChar) => currentChar === testChar)
+    .length;
+}
+
+/*
+
+### Using `Array.prototype.reduce()`
+
+function countOccurrences(str, char) {
+  return Array.from(str).reduce((count, currentChar) => {
+    return currentChar === char ? count + 1 : count;
+  }, 0);
+}
+
+### Using `Array.prototype.filter()`
+
+function countOccurrences(str, char) {
+  return Array.from(str).filter(currentChar => currentChar === char).length;
+}
+
+### Using `String.prototype.split()`
+
+function countOccurrences(str, char) {
+  return str.split(char).length - 1;
+}
+
+
+*/
 
 module.exports = countOccurrences;
--- WillNotes.md
+++ WillNotes.md
@@ -6,6 +6,6 @@ use code runner extension right click run...
 
 change file name from -test to .test in order to run
 
-the npm test in terminal
+then npm test in terminal
 
 https://www.traversymedia.com/products/70-javascript-challenges-using-data-structures-algorithms/categories/2153683525/posts/2170879655
