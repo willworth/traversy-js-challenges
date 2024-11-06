8c6c32167a4e0a1c84a43ea4a2c9cdba16368172
Will Worth <will.worth@accenture.com>
2024-11-06 09:36:26 +0100

    p

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,22 @@
-function isPalindrome() {}
+function isPalindrome(word) {
+
+  // strip special characters
+  word = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+let reversedWord = word.split('').reverse().join('');
+if (word === reversedWord) {
+  return true;
+}
 
 module.exports = isPalindrome;
+
+
+
+// ```js
+// function isPalindrome(str) {
+//   const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+//   const reversedStr = formattedStr.split('').reverse().join('');
+//   return formattedStr === reversedStr;
+// }
+// ```
+
