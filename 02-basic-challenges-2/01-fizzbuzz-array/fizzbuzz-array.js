1f819be32095ff022be4c98465fb8745066d901b
Will Worth <will.worth@accenture.com>
2024-11-15 14:20:27 +0100

    fizzbuzz omg

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
-
+function fizzBuzzArray(num) {
+  console.log(num)
+  let result = [];
+  for(i=1; i<=num; i++){
+    if(i%3==0 && i%5==0){
+      result.push('FizzBuzz');
+    } else if(i%3==0){
+      result.push('Fizz');
+    } else if(i%5==0){
+      result.push('Buzz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result
+}
 module.exports = fizzBuzzArray;
