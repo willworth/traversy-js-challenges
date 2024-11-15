d5d938ce9829b510efbdcfef60ffd548570b055e
Will Worth <will.worth@accenture.com>
2024-11-15 14:41:35 +0100

    array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,7 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+
+  const intersection = arr1.filter(value => arr2.includes(value));
+  return intersection;
+}
 
 module.exports = arrayIntersection;
